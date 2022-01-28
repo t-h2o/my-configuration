8cdb489016e1e5a8807f7c87feebe1aa6474d0eb
theo-grivel <grivel.theo@protonmail.com>
2022-01-28 09:36:33 +0100

    install unsinstall script

--- both/nvim/install.sh
+++ both/nvim/install.sh
@@ -5,17 +5,17 @@ sudo apt install neovim
 
 ###	Install plugin for neovim
 
-DIR=$HOME"/.local/share/nvim/site/autoload/"
-URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+#DIR=$HOME"/.local/share/nvim/site/autoload/"
+#URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
-mkdir -p $DIR
-wget $URL -O $DIR"plug.vim"
+#mkdir -p $DIR
+#wget $URL -O $DIR"plug.vim"
 
 
 ###	Move configuration files in the good directories
 
 DIR=$HOME"/.config"
-mv ./nvim $DIR/
+cp -r ./nvim $DIR/
 
 
 ###	Install plug in Neovim
