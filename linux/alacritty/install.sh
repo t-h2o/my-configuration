79439403e956a21d19d02343a0489431ff190af3
theo-grivel <grivel.theo@protonmail.com>
2022-02-03 10:53:27 +0100

    download the default config with all comments

--- linux/alacritty/install.sh
+++ linux/alacritty/install.sh
@@ -1,6 +1,7 @@
  sudo add-apt-repository ppa:mmstick76/alacritty
  sudo apt update
  sudo apt install alacritty
-
+mkdir -p $HOME/.config/alacritty
+wget https://raw.githubusercontent.com/alacritty/alacritty/master/alacritty.yml $HOME/.config/alacritty/
 #	move configuration
-mv alacritty $HOME/.config/
+# mv alacritty $HOME/.config/
