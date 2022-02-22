1690f4251c7125fca5c7e4a05f9c6d3ebf425665
To grivel <grivel.theo@protonmail.com>
2022-02-22 10:07:39 +0100

    new wm

---
+++ arch/bspwm/install.sh
@@ -0,0 +1,5 @@
+pacman -Su bspwm sxhkd
+
+mkdir -p .config/{bspwm,sxhkd}
+theo@foxtrot:~$ cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+theo@foxtrot:~$ cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd
