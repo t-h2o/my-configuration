cd3ad384e5e81e0783ac55456c4028ab2e0b4142
|°_°| <grivel.theo@protonmail.com>
2022-03-16 15:22:31 +0100

    wlan leak
    need to delete the repository

--- arch/problems/iwctl_not_working.sh
+++ arch/problems/iwctl_not_working.sh
@@ -1,5 +1,8 @@
+MY_PASSWD="my_passwd"
+MY_WLAN="my_wlan"
+
 systemctl start NetworkManager
 systemctl enable NetworkManager
 systemctl status NetworkManager
 nmcli device wifi list
-nmcli device wifi connect dont_connect_2400MHZ password bondaccord2400
+nmcli device wifi connect $MY_WLAN password $MY_PASSWD
