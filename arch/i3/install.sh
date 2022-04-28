3a830df90258b19df1ea130407ae68d7b2581a9a
my name <my email>
2022-04-29 01:03:36 +0200

    i3 improve

--- arch/i3/install.sh
+++ arch/i3/install.sh
@@ -1,11 +1,10 @@
 ### Install packages
 
-pacman -S xorg xorg-xinit i3-wm i3lock i3status i3blocks dmenu
+pacman -S xorg-xinit i3-wm i3status dmenu lightdm lightdm-gtk-greeter
 
 
 ### Mode files
 
 cp ./Assets/xinitrc ~/.xinitrc
-# cp /etc/X11/xinit/xinitrc ~/.xinitrc
 mkdir -p $HOME/.config/i3
 cp ./Assets/config $HOME/.config/i3/
