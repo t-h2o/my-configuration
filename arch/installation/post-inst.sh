30f52fb153c6384d43e92e8a1e94e141a134935f
my name <my email>
2022-04-29 01:03:36 +0200

    i3 improve

--- arch/installation/post-inst.sh
+++ arch/installation/post-inst.sh
@@ -9,6 +9,13 @@ systemctl start NetworkManager
 #EDITOR=vim visudo /etc/sudoers
 
 
+# Firefox
+pacman -S firefox
+# Choose this settings
+## 2) pipewire-jack
+## 1) pipewire-media-session
+## 1) gnu-free-fonts
+
 # Audio
 
 pacman -S pipewire pipewire-docs pipewire-alsa
