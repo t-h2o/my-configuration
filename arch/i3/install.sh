35cbef0cfb556fda4e490e0c88ab081be535dcf1
To grivel <grivel.theo@protonmail.com>
2022-05-24 22:57:34 +0200

    change few things I3

--- arch/i3/install.sh
+++ arch/i3/install.sh
@@ -1,10 +1,14 @@
 ### Install packages
 
-pacman -S xorg-xinit i3-wm i3status dmenu lightdm lightdm-gtk-greeter
-
+pacman -S xorg-xinit    \
+	xorg-server         \
+	i3-wm               \
+	i3status            \
+	dmenu lightdm       \
+	lightdm-gtk-greeter
 
 ### Mode files
 
-cp ./Assets/xinitrc ~/.xinitrc
+cp xinitrc ~/.xinitrc
 mkdir -p $HOME/.config/i3
-cp ./Assets/config $HOME/.config/i3/
+cp config $HOME/.config/i3/
