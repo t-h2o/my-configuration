3fb4331458d837813115904f8d1e224d5dd30156
To grivel <grivel.theo@protonmail.com>
2022-09-01 10:32:34 +0200

    vim: updated install script

--- arch/vimrc/install.sh
+++ arch/vimrc/install.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
 
-#!/bin/bash
+###	Move the dot files
 move_dot_files () {
-	cp ./vimrc $HOME/.vimrc
-	cp -r ./vim $HOME/.config/vim
+	
+	cp ./vimrc "$HOME"/.vimrc
+	cp -r ./vim "$HOME"/.config/vim
+	
 }
 
 ###	Install plugin manager
@@ -11,23 +14,27 @@ install_plugin () {
 	DIR=$HOME"/.vim/autoload/"
 	URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 	echo Download from : $URL
-	echo in : $DIR
-	mkdir -p $DIR
-	curl $URL > $DIR"plug.vim"
-}
-
-install_cyberpunk () {
-	mkdir -p ~/.vim/colors
-	PATH_DOWNLOAD=$HOME/.vim/colors
-	LINK="https://raw.githubusercontent.com/Roboron3042/Cyberpunk-Neon/master/terminal-apps/vim/cyberpunk-neon.vim"
+	echo in : "$DIR"
+	mkdir -p "$DIR"
+	curl $URL > "$DIR""plug.vim"
 	
-	curl $LINK > $PATH_DOWNLOAD/cyberpunk-neon.vim
 }
 
-
+###	Install a color theme
+install_cyberpunk () {
+	
+	mkdir -p ~/.vim/colors
+	PATH_DOWNLOAD="$HOME"/.vim/colors
+	LINK="https://raw.githubusercontent.com/Roboron3042/Cyberpunk-Neon/master/terminal-apps/vim/cyberpunk-neon.vim"
+	curl "$LINK" > "$PATH_DOWNLOAD"/cyberpunk-neon.vim
+	
+}
 
 #	MAIN
+main () {
 
-install_plugin
-move_dot_files
-install_cyberpunk
+	install_plugin
+	move_dot_files
+	# install_cyberpunk
+
+}
