8c638669a92d53f435c838e3c8ad97ab66e75241
theo grivel <grivel.theo@protonmail.com>
2023-08-23 11:33:47 +0200

    other commands

--- xdg-open.sh
+++ xdg-open.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# get the filetype of a filetype
+xdg-mime query filetype README.adoc
+
+# get the default program of a filetype
+xdg-mime query default text/plain
+
 # vim as default program
 xdg-mime default vim.desktop text/plain
